83da59f8ed411209cb45d6af234a700c4bd53a19
juaniike <ignaciojuan975@gmail.com>
2025-06-18 21:02:33 -0300

    Lista de tareas-vercelJson.

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})
 
-  // ...otras config...
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true,
-  },
-});
